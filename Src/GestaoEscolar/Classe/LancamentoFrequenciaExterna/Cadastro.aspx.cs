86a809928079225cfe41654a0e6eff250885e1b9
Daniel Jun Suguimoto <daniel.suguimoto@mstech.com.br>
2017-02-20 09:48:03 -0300

    20000006-566 #3 Adicionado webforms para a tela de lançamento de frequencia externa e os scripts para adicionar o módulo no sistema.

---
+++ Src/GestaoEscolar/Classe/LancamentoFrequenciaExterna/Cadastro.aspx.cs
@@ -0,0 +1,23 @@
+﻿namespace GestaoEscolar.Classe.LancamentoFrequenciaExterna
+{
+    using MSTech.GestaoEscolar.Web.WebProject;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+
+
+    public partial class Cadastro : MotherPageLogado
+    {
+        #region Page Life Cicle
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion Page Life Cicle
+    }
+}
