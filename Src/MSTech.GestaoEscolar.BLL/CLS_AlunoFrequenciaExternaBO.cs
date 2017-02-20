8e6bb509b333fd4f317f93ecaec7cd176e57a80b
Daniel Jun Suguimoto <daniel.suguimoto@mstech.com.br>
2017-02-20 09:14:39 -0300

    20000006-566 #3 Adicionado classes da tabela CLS_AlunoFrequenciaExterna

---
+++ Src/MSTech.GestaoEscolar.BLL/CLS_AlunoFrequenciaExternaBO.cs
@@ -0,0 +1,18 @@
+/*
+	Classe gerada automaticamente pelo MSTech Code Creator
+*/
+
+namespace MSTech.GestaoEscolar.BLL
+{
+	using MSTech.Business.Common;
+	using MSTech.GestaoEscolar.Entities;
+	using MSTech.GestaoEscolar.DAL;
+	
+	/// <summary>
+	/// Description: CLS_AlunoFrequenciaExterna Business Object. 
+	/// </summary>
+	public class CLS_AlunoFrequenciaExternaBO : BusinessBase<CLS_AlunoFrequenciaExternaDAO, CLS_AlunoFrequenciaExterna>
+	{
+				
+	}
+}
