0253035fbe30048ead2beb9354d738de6c72b72e
Márcia Haga <marcia.haga@mstech.com.br>
2017-03-20 15:14:40 -0300

    20000006-685 #19 - Ajuste no script inicial para controle da execução do serviço de processamento da sugestão de aulas previstas.

--- Scripts/GestaoAcademica/1.68.0.0/109-GestaoEscolar_InsereSugestaoAulasPrevistas2017.sql
+++ Scripts/GestaoAcademica/1.68.0.0/109-GestaoEscolar_InsereSugestaoAulasPrevistas2017.sql
@@ -5,20 +5,25 @@ GO
 BEGIN TRANSACTION
 SET XACT_ABORT ON
 
-	DECLARE @dataInicio DATETIME = GETDATE();
+	IF (NOT EXISTS (SELECT TOP 1 1 FROM SYS_ServicosLogExecucao WITH(NOLOCK) WHERE ser_id = 52))
+	BEGIN
 
-	-- Executa o serviço para as agendas geradas a partir de 2017
-	EXEC MS_JOB_ProcessamentoSugestaoAulasPrevistas 0
+		DECLARE @dataInicio DATETIME = GETDATE();
 
-	-- Insere registros na tabela SYS_ServicosLogExecucao
-	-- para referência de data na próxima execução do serviço.
-	DECLARE @sle_id1 UNIQUEIDENTIFIER, @sle_id2 UNIQUEIDENTIFIER
+		-- Executa o serviço para as agendas geradas a partir de 2017
+		EXEC MS_JOB_ProcessamentoSugestaoAulasPrevistas 0
 
-	INSERT INTO SYS_ServicosLogExecucao (ser_id, sle_dataInicioExecucao, sle_dataFimExecucao)
-	VALUES (52, @dataInicio, GETDATE())
+		-- Insere registros na tabela SYS_ServicosLogExecucao
+		-- para referência de data na próxima execução do serviço.
+		DECLARE @sle_id1 UNIQUEIDENTIFIER, @sle_id2 UNIQUEIDENTIFIER
 
-	INSERT INTO SYS_ServicosLogExecucao (ser_id, sle_dataInicioExecucao, sle_dataFimExecucao)
-	VALUES (53, @dataInicio, GETDATE())
+		INSERT INTO SYS_ServicosLogExecucao (ser_id, sle_dataInicioExecucao, sle_dataFimExecucao)
+		VALUES (52, @dataInicio, GETDATE())
+
+		INSERT INTO SYS_ServicosLogExecucao (ser_id, sle_dataInicioExecucao, sle_dataFimExecucao)
+		VALUES (53, @dataInicio, GETDATE())
+
+	END
 	
 -- Fechar transação	
 SET XACT_ABORT OFF
