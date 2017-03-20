88ed720d5cf7bd59594fe43b331e2b3c4f32f736
Leonardo Andrade Brito <leonardo.brito@mstech.com.br>
2017-03-20 15:40:21 -0300

    Correção nos scripts para seguir a ordem correta e separar os menus. #20000006-749 #96

--- Scripts/GestaoAcademica/1.68.0.0/102-CoreSSO_Novos_SiteMap.sql
+++ Scripts/GestaoAcademica/1.68.0.0/102-CoreSSO_Novos_SiteMap.sql
@@ -6,7 +6,6 @@ BEGIN TRANSACTION
 SET XACT_ABORT ON
 
 	DECLARE @nomeSistema VARCHAR(100) = ' SGP'
-	DECLARE @sis_id INT = (SELECT TOP 1 sis_id FROM SYS_Sistema WHERE sis_nome = @nomeSistema)
 
 	/***************
 		Copiar do exemplo abaixo.
@@ -20,14 +19,6 @@ SET XACT_ABORT ON
 		,@SiteMapUrl = '~/Configuracao/TipoEvento/Busca.aspx' -- Url da SiteMap (Obrigatório)
 	*/
 	
-	DECLARE @mod_id INT = (SELECT TOP 1 mod_id FROM SYS_Modulo WITH(NOLOCK) WHERE mod_nome = 'Áreas' AND sis_id = @sis_id AND mod_situacao <> 3)
-	UPDATE SYS_ModuloSitemap
-	SET msm_nome = 'Cadastro de links/documentos'
-	WHERE mod_id = @mod_id AND sis_id = @sis_id AND msm_nome = 'Cadastro de documentos'
-	UPDATE SYS_ModuloSitemap
-	SET msm_nome = 'Cadastro de documentos'
-	WHERE mod_id = @mod_id AND sis_id = @sis_id AND msm_nome = 'Cadastro de áreas para links e documentos.'
-
 -- Fechar transação
 SET XACT_ABORT OFF
 COMMIT TRANSACTION
