0bdd3fe38a0d684074f2f008db7c8925d0339375
leticia.goes <leticia.goes@mstech.com.br>
2017-03-29 12:44:21 -0300

    20000006-815 #154 alteração no titulo do rdl e scripts de menu, cfg relatorio e resource

--- Scripts/GestaoAcademica/1.69.0.0/106-GestaoEscolar_Insere_Resources.sql
+++ Scripts/GestaoAcademica/1.69.0.0/106-GestaoEscolar_Insere_Resources.sql
@@ -102,6 +102,13 @@ SET XACT_ABORT ON
             , @rcr_codigo = 0 
             , @rcr_valorPadrao = 'Existe divergência entre aulas criadas e aulas previstas'
 
+	EXEC MS_InsereResource 
+            @rcr_chave = 'RelatoriosCP.DadosAlunosJustificativaFalta.Busca.btnGerarRel.Text' 
+            , @rcr_NomeResource = 'Relatorios'
+            , @rcr_cultura = 'pt-BR'
+            , @rcr_codigo = 0 
+            , @rcr_valorPadrao = 'Gerar documento'
+
 -- Fechar transação     
 SET XACT_ABORT OFF 
 COMMIT TRANSACTION
