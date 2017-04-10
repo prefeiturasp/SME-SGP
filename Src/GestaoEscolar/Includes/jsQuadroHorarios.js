953d233cfdd3d0b3a3e6474b946e3b3be78d86ec
Ana Cecilia Nascimento <ana.nascimento@mstech.com.br>
2017-04-10 13:50:46 -0300

    Resolvendo conflitos

---
+++ Src/GestaoEscolar/Includes/jsQuadroHorarios.js
@@ -0,0 +1,43 @@
+﻿function jsQuadroHorarios() {
+    createDialog('.divAtribuirDisciplina', 600, 0);
+}
+
+function clickHorario(calEvent) {
+    if (calEvent.id.split(';')[5] == 1)
+        atribuirDisciplina(calEvent);
+    else
+        visualizaHorario(calEvent);
+}
+
+function atribuirDisciplina(calEvent) {
+    $('span#ui-dialog-title-divAtribuirDisciplina').text('Atribuição de disciplina ao horário');
+    $('#hdnHorario').val(calEvent.id);
+
+    $('#txtDiaSemana').val($('#hdnDiasSemana').val().split(';')[calEvent.id.split(';')[3] - 1]);
+
+    $('.txtHoraInicial').val(calEvent.start.format('HH:mm'));
+    $('.txtHoraFinal').val(calEvent.end.format('HH:mm'));
+    $('div.ddlTipoHorario select').val(calEvent.id.split(';')[2]);
+
+    $('div.ddlTurmaDisciplina').show();
+    $('#btnAtribuiDisciplina').show();
+    $('div.ddlTurmaDisciplina select').val(calEvent.id.split(';')[4]);    
+
+    $(document).ready(function () { $('.divAtribuirDisciplina').dialog('open'); $('.ddlTurmaDisciplina').first().focus(); return false; });
+}
+
+function visualizaHorario(calEvent) {
+    $('span#ui-dialog-title-divAtribuirDisciplina').text('Visualizar horário');
+    $('#txtDiaSemana').val($('#hdnDiasSemana').val().split(';')[calEvent.id.split(';')[3] - 1]);
+    $('.txtHoraInicial').val(calEvent.start.format('HH:mm'));
+    $('.txtHoraFinal').val(calEvent.end.format('HH:mm'));
+    $('div.ddlTipoHorario select').val(calEvent.id.split(';')[2]);
+    $('div.ddlTurmaDisciplina').hide();
+    $('#btnAtribuiDisciplina').hide();
+    
+    $(document).ready(function () { $('.divAtribuirDisciplina').dialog('open'); return false; });
+}
+
+// Insere as funções na lista de funcões - será chamado no Init.js.
+arrFNC.push(jsQuadroHorarios);
+arrFNCSys.push(jsQuadroHorarios);
