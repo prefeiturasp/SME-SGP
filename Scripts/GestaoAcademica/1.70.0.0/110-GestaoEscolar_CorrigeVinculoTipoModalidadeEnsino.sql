3aadee635d06aefbc5db9556ecc21e1616523a0d
Daniel Jun Suguimoto <daniel.suguimoto@mstech.com.br>
2017-04-11 11:39:02 -0300

    20000006-957 #198 Adicionado scripts para corrigir as modalidades de ensino dos cursos e para parametrizar os tipos que não devem ser exibidos no sistema.

---
+++ Scripts/GestaoAcademica/1.70.0.0/110-GestaoEscolar_CorrigeVinculoTipoModalidadeEnsino.sql
@@ -0,0 +1,66 @@
+USE [GestaoPedagogica]
+GO
+
+BEGIN TRANSACTION
+SET XACT_ABORT ON
+	
+	DECLARE @tme_idEJA INT;
+	SELECT
+		@tme_idEJA = tme_id
+	FROM
+		ACA_TipoModalidadeEnsino WITH(NOLOCK)
+	WHERE
+		tme_nome = 'Educação de Jovens e Adultos - EJA'
+		AND tme_idSuperior IS NULL
+		AND tme_situacao <> 3
+
+	DECLARE @tme_idEJAReg INT;
+	SELECT
+		@tme_idEJAReg = tme_id
+	FROM
+		ACA_TipoModalidadeEnsino WITH(NOLOCK)
+	WHERE
+		tme_nome = 'EJA Regular'
+		AND tme_idSuperior = @tme_idEJA
+		AND tme_situacao <> 3
+
+	DECLARE @tme_idEJAMod INT;
+	SELECT
+		@tme_idEJAMod = tme_id
+	FROM
+		ACA_TipoModalidadeEnsino WITH(NOLOCK)
+	WHERE
+		tme_nome = 'EJA Modular'
+		AND tme_idSuperior = @tme_idEJA
+		AND tme_situacao <> 3
+
+	DECLARE @tme_idCIEJA INT;
+	SELECT
+		@tme_idCIEJA = tme_id
+	FROM
+		ACA_TipoModalidadeEnsino WITH(NOLOCK)
+	WHERE
+		tme_nome = 'CIEJA'
+		AND tme_idSuperior = @tme_idEJA
+		AND tme_situacao <> 3
+
+	UPDATE ACA_Curso
+	SET tme_id = @tme_idEJAReg
+	WHERE tme_id = @tme_idEJA
+	AND cur_nome_abreviado = 'EJA'
+	AND cur_situacao <> 3
+	
+	UPDATE ACA_Curso
+	SET tme_id = @tme_idEJAMod
+	WHERE tme_id = @tme_idEJA
+	AND cur_nome_abreviado = 'EJA MOD'
+	AND cur_situacao <> 3
+
+	UPDATE ACA_Curso
+	SET tme_id = @tme_idCIEJA
+	WHERE tme_id = @tme_idEJA
+	AND cur_nome_abreviado = 'CIEJA'
+	AND cur_situacao <> 3
+
+SET XACT_ABORT OFF
+COMMIT TRANSACTION
