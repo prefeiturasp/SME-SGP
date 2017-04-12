8299d14400c2716d9a985420c8b9cf25ad3dc362
Ana Cecilia Nascimento <ana.nascimento@mstech.com.br>
2017-04-12 17:31:17 -0300

    20000006-1002 Cadastro de sondagem - Corrigir escrita de Subquestão

--- Scripts/GestaoAcademica/1.70.0.0/106-GestaoEscolar_Insere_Resources.sql
+++ Scripts/GestaoAcademica/1.70.0.0/106-GestaoEscolar_Insere_Resources.sql
@@ -261,7 +261,7 @@ SET XACT_ABORT ON
 		, @rcr_NomeResource = 'Academico'
 		, @rcr_cultura = 'pt-BR'
 		, @rcr_codigo = 0 
-		, @rcr_valorPadrao = 'Sub-Questões'
+		, @rcr_valorPadrao = 'Subquestões'
 
 	EXEC MS_InsereResource 
 		@rcr_chave = 'Sondagem.Cadastro.lblLegendRespostas.Text' 
