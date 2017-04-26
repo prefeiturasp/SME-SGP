ad7a605425d4fde63ac5a1e44fd1429ee9f3da3d
Leonardo Andrade Brito <leonardo.brito@mstech.com.br>
2017-04-26 13:24:16 -0300

    Criado pop-up de confirmação de cancelamento de agendamento. #20000006-1080 #143

---
+++ Src/GestaoEscolar/Includes/jsMsgConfirmCancelamentoAgendamento.js
@@ -0,0 +1,5 @@
+﻿
+var btnAction="";var executeExcluir=false;function jsMsgConfirm(){$('.btCancelarAgendamento').die('click').live('click',function(){btnAction="#"+this.id;if($("#divConfirm").length>0)
+$("#divConfirm").remove();$("<div id=\"divConfirm\" title=\"Confirmação\">Confirma o cancelamento do agendamento?</div>").dialog({bgiframe:true,autoOpen:false,resizable:false,modal:true,buttons:{"Sim":function(){$(this).dialog("close");executeExcluir=true;$(btnAction).click();},"Não":function(){$(this).dialog("close");}}});if(!executeExcluir){$("#divConfirm").dialog("open");}
+var result=executeExcluir;executeExcluir=false;return result;});}
+arrFNC.push(jsMsgConfirm);
