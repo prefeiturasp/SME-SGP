bc4290178d0e44f906034cd75189c16cd9dad415
Márcia Haga <marcia.haga@mstech.com.br>
2017-05-11 18:22:59 -0300

    20000006-1142 #264 - Adicionada API que retorna os dados da sondagem para o aluno.

---
+++ Src/MSTech.GestaoEscolar.ObjetosSincronizacao/DTO/Entrada/AnoAlunoEntradaDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MSTech.GestaoEscolar.ObjetosSincronizacao.DTO.Entrada
+{
+    public class AnoAlunoEntradaDTO
+    {
+        public int ano { get; set; }
+        public long alu_id { get; set; }
+    }
+}
---
+++ Src/MSTech.GestaoEscolar.ObjetosSincronizacao/DTO/Saida/SondagemSaidaDTO.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace MSTech.GestaoEscolar.ObjetosSincronizacao.DTO.Saida
+{
+    public class SondagemSaidaDTO
+    {
+        public int Status { get; set; }
+        public string StatusDescription { get; set; }
+        public string Date { get; set; }
+
+        public List<SondagemDTO> sondagens { get; set; }
+    }
+
+    public class SondagemDTO
+    {
+        public int id { get; set; }
+        public string titulo { get; set; }
+        public string descricao { get; set; }
+        public List<QuestaoDTO> questoes { get; set; }
+        public List<QuestaoDTO> subQuestoes { get; set; }
+        public List<RespostaDTO> respostas { get; set; }
+        public List<AgendamentoDTO> agendamentos { get; set; }
+    }
+
+    public class QuestaoDTO
+    {
+        public int id { get; set; }
+        public string descricao { get; set; }
+        public int ordem { get; set; }
+    }
+
+    public class RespostaDTO
+    {
+        public int id { get; set; }
+        public string sigla { get; set; }
+        public string descricao { get; set; }
+        public int orderm { get; set; }
+    }
+
+    public class AgendamentoDTO
+    {
+        public int id { get; set; }
+        public string dataInicio { get; set; }
+        public string dataFim { get; set; }
+        List<RespostaAlunoDTO> respostasAluno { get; set; }
+    }
+
+    public class RespostaAlunoDTO
+    {
+        public int idQuestao { get; set; }
+        public int idSubQuestao { get; set; }
+        public int idResposta { get; set; }
+    }
+}
