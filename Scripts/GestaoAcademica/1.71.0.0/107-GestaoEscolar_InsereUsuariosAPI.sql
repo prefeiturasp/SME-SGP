11b27d38294566fe04d3ee627b3cd1e6c9baca62
Daniel Jun Suguimoto <daniel.suguimoto@mstech.com.br>
2017-05-15 10:57:08 -0300

    20000006-1141 #260 Ajustes scripts para a consulta de parecer conclusivo (WebApi)

---
+++ Scripts/GestaoAcademica/1.71.0.0/107-GestaoEscolar_InsereUsuariosAPI.sql
@@ -0,0 +1,41 @@
+USE [GestaoPedagogica]
+GO
+
+BEGIN TRANSACTION 
+SET XACT_ABORT ON   
+
+	DECLARE @usuario VARCHAR(100) = 'usuarioGestaoPedagogicaAPI';
+	-- Alterar senha -- Criptografada
+	DECLARE @senha VARCHAR(256) = '0lGzDU+IVMg='
+
+	IF (NOT EXISTS (SELECT TOP 1 1
+					FROM SYS_UsuarioAPI WITH(NOLOCK)
+					WHERE uap_usuario = @usuario
+					AND uap_situacao <> 3))
+	BEGIN
+		DECLARE @dataAtual DATETIME = GETDATE();
+
+		INSERT INTO SYS_UsuarioAPI
+		(
+			uap_usuario,
+			uap_senha,
+			uap_situacao,
+			uap_dataCriacao,
+			uap_dataAlteracao
+		)
+		VALUES
+		(
+			@usuario,
+			@senha,
+			1,
+			@dataAtual,
+			@dataAtual
+		)
+	END
+
+-- Fechar transação     
+SET XACT_ABORT OFF 
+COMMIT TRANSACTION
+
+
+
