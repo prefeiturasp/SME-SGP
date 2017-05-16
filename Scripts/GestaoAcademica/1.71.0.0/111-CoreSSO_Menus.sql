2dedb2ede831b10ac3c8087badab4088e658d175
Leonardo Andrade Brito <leonardo.brito@mstech.com.br>
2017-05-16 11:29:03 -0300

    Criado script para reorganizar o menu. #20000006-1155 #249

---
+++ Scripts/GestaoAcademica/1.71.0.0/111-CoreSSO_Menus.sql
@@ -0,0 +1,246 @@
+USE [CoreSSO]
+GO
+
+BEGIN TRANSACTION 
+SET XACT_ABORT ON   
+
+	DECLARE @sis_id INT = 102
+	DECLARE @visaoAdm INT = 1
+	DECLARE @visaoGestao INT = 2
+	DECLARE @visaoUnidade INT = 3
+	DECLARE @nomeSistema VARCHAR(100) = ' SGP'
+
+	UPDATE SYS_Modulo
+	SET mod_nome = 'Sistemas'
+	WHERE mod_nome = 'Configuração' AND sis_id = 102 AND mod_situacao <> 3
+	
+	UPDATE SYS_Modulo
+	SET mod_nome = 'Calendário'
+	WHERE mod_nome = 'Calendário escolar' AND sis_id = 102 AND mod_situacao <> 3
+	
+	UPDATE SYS_Modulo
+	SET mod_nome = 'Turmas normais'
+	WHERE mod_nome = 'Manutenção de turmas' AND sis_id = 102 AND mod_situacao <> 3
+	
+	UPDATE SYS_Modulo
+	SET mod_nome = 'Turmas eletivas'
+	WHERE mod_nome = 'Manutenção de turmas eletivas' AND sis_id = 102 AND mod_situacao <> 3
+	
+	UPDATE SYS_Modulo
+	SET mod_nome = 'Turmas multisseriadas'
+	WHERE mod_nome = 'Manutenção de turmas multisseriadas' AND sis_id = 102 AND mod_situacao <> 3
+	
+	EXEC MS_InserePaginaMenu
+		@nomeSistema = @nomeSistema -- Nome do sistema (obrigatório)
+		,@nomeModuloAvo = NULL -- Nome do módulo avó (Opcional, apenas quando houver) 
+		,@nomeModuloPai = NULL -- Nome do módulo pai (Opcional, apenas quando houver)
+		,@nomeModulo = 'Configurações' -- Nome do módulo (Obrigatório)
+		,@SiteMap1Nome = 'Configurações'
+		,@SiteMap1Url = NULL
+		,@SiteMap2Nome = NULL
+		,@SiteMap2Url = NULL
+		,@SiteMap3Nome = NULL
+		,@SiteMap3Url = NULL
+		,@possuiVisaoAdm = 1 -- Indicar se possui visão de administador
+		,@possuiVisaoGestao = 1 -- Indicar se possui visão de Gestão
+		,@possuiVisaoUA = 1 -- Indicar se possui visão de UA
+		,@possuiVisaoIndividual = 0 -- Indicar se possui visão de individual
+
+	EXEC MS_InserePaginaMenu
+		@nomeSistema = @nomeSistema -- Nome do sistema (obrigatório)
+		,@nomeModuloAvo = NULL -- Nome do módulo avó (Opcional, apenas quando houver) 
+		,@nomeModuloPai = 'Administração' -- Nome do módulo pai (Opcional, apenas quando houver)
+		,@nomeModulo = 'Calendário escolar' -- Nome do módulo (Obrigatório)
+		,@SiteMap1Nome = 'Calendário escolar'
+		,@SiteMap1Url = NULL
+		,@SiteMap2Nome = NULL
+		,@SiteMap2Url = NULL
+		,@SiteMap3Nome = NULL
+		,@SiteMap3Url = NULL
+		,@possuiVisaoAdm = 1 -- Indicar se possui visão de administador
+		,@possuiVisaoGestao = 1 -- Indicar se possui visão de Gestão
+		,@possuiVisaoUA = 1 -- Indicar se possui visão de UA
+		,@possuiVisaoIndividual = 1 -- Indicar se possui visão de individual
+
+	EXEC MS_InserePaginaMenu
+		@nomeSistema = @nomeSistema -- Nome do sistema (obrigatório)
+		,@nomeModuloAvo = NULL -- Nome do módulo avó (Opcional, apenas quando houver) 
+		,@nomeModuloPai = 'Administração' -- Nome do módulo pai (Opcional, apenas quando houver)
+		,@nomeModulo = 'Manutenção de turmas' -- Nome do módulo (Obrigatório)
+		,@SiteMap1Nome = 'Manutenção de turmas'
+		,@SiteMap1Url = NULL
+		,@SiteMap2Nome = NULL
+		,@SiteMap2Url = NULL
+		,@SiteMap3Nome = NULL
+		,@SiteMap3Url = NULL
+		,@possuiVisaoAdm = 1 -- Indicar se possui visão de administador
+		,@possuiVisaoGestao = 1 -- Indicar se possui visão de Gestão
+		,@possuiVisaoUA = 1 -- Indicar se possui visão de UA
+		,@possuiVisaoIndividual = 1 -- Indicar se possui visão de individual
+
+	
+	-- Mudando módulos de Configurações para o mod_idPai novo
+
+	DECLARE @mod_idPai INT = (SELECT mod_id FROM SYS_Modulo WHERE mod_nome = 'Configurações' AND sis_id = @sis_id AND mod_situacao = 1)
+
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Formato de avaliação'
+
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Escala de avaliação'
+
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Turnos'
+	
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Cursos'
+	
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Orientações curriculares'
+	
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Reuniões de responsáveis'
+	
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Nível de aprendizado'
+	
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Matriz de habilidades'
+	
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Objetos de conhecimento'
+	
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Sondagem'
+	
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Configuração do serviço de pendência'
+	
+	UPDATE SYS_Modulo
+	SET mod_situacao = 3
+	WHERE sis_id = @sis_id AND mod_nome = 'Cadastros'
+	
+	-- Mudando módulos de Calendário para o mod_idPai novo
+
+	SET @mod_idPai = (SELECT mod_id FROM SYS_Modulo WHERE mod_nome = 'Calendário escolar' AND sis_id = @sis_id AND mod_situacao = 1)
+
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Calendário'
+
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Eventos do calendário escolar'
+
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Abertura de anos letivos anteriores'
+	
+	-- Organizando a ordem dos módulos 
+	
+	UPDATE SYS_VisaoModuloMenu
+	SET vmm_ordem = 2
+	WHERE sis_id = @sis_id
+	AND vis_id = @visaoAdm
+	AND mod_id = @mod_idPai
+
+	UPDATE SYS_VisaoModuloMenu
+	SET vmm_ordem = 2
+	WHERE sis_id = @sis_id
+	AND vis_id = @visaoGestao
+	AND mod_id = @mod_idPai
+
+	UPDATE SYS_VisaoModuloMenu
+	SET vmm_ordem = 4
+	WHERE sis_id = @sis_id
+	AND vis_id = @visaoUnidade
+	AND mod_id = @mod_idPai
+	
+	-- Mudando módulos de Manutenção de turmas para o mod_idPai novo
+
+	SET @mod_idPai = (SELECT mod_id FROM SYS_Modulo WHERE mod_nome = 'Manutenção de turmas' AND sis_id = @sis_id AND mod_situacao = 1)
+
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Turmas normais'
+
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Turmas eletivas'
+
+	UPDATE SYS_Modulo 
+	SET mod_idPai = @mod_idPai
+	WHERE sis_id = @sis_id AND mod_nome = 'Turmas multisseriadas'
+	
+	-- Organizando a ordem dos módulos 
+
+	DECLARE @vmm_ordem INT
+	
+	SELECT @vmm_ordem = MAX(vmm_ordem)
+	FROM SYS_Modulo mdl WITH(NOLOCK)
+	INNER JOIN SYS_Modulo mdf WITH(NOLOCK)
+		ON mdl.sis_id = mdf.sis_id
+		AND mdl.mod_id = mdf.mod_idPai
+		AND mdf.mod_situacao <> 3
+	INNER JOIN SYS_VisaoModuloMenu vmm WITH(NOLOCK)
+		ON mdf.sis_id = vmm.sis_id
+		AND mdf.mod_id = vmm.mod_id
+		AND vis_id = @visaoAdm
+	WHERE mdl.sis_id = @sis_id AND mdl.mod_nome = 'Administração' AND mdl.mod_situacao <> 3
+	
+	UPDATE SYS_VisaoModuloMenu
+	SET vmm_ordem = ISNULL(@vmm_ordem + 1, 1)
+	WHERE sis_id = @sis_id
+	AND vis_id = @visaoAdm
+	AND mod_id = @mod_idPai
+
+	SELECT @vmm_ordem = MAX(vmm_ordem)
+	FROM SYS_Modulo mdl WITH(NOLOCK)
+	INNER JOIN SYS_Modulo mdf WITH(NOLOCK)
+		ON mdl.sis_id = mdf.sis_id
+		AND mdl.mod_id = mdf.mod_idPai
+		AND mdf.mod_situacao <> 3
+	INNER JOIN SYS_VisaoModuloMenu vmm WITH(NOLOCK)
+		ON mdf.sis_id = vmm.sis_id
+		AND mdf.mod_id = vmm.mod_id
+		AND vis_id = @visaoGestao
+	WHERE mdl.sis_id = @sis_id AND mdl.mod_nome = 'Administração' AND mdl.mod_situacao <> 3
+	
+	UPDATE SYS_VisaoModuloMenu
+	SET vmm_ordem = ISNULL(@vmm_ordem + 1, 1)
+	WHERE sis_id = @sis_id
+	AND vis_id = @visaoGestao
+	AND mod_id = @mod_idPai
+
+	SELECT @vmm_ordem = MAX(vmm_ordem)
+	FROM SYS_Modulo mdl WITH(NOLOCK)
+	INNER JOIN SYS_Modulo mdf WITH(NOLOCK)
+		ON mdl.sis_id = mdf.sis_id
+		AND mdl.mod_id = mdf.mod_idPai
+		AND mdf.mod_situacao <> 3
+	INNER JOIN SYS_VisaoModuloMenu vmm WITH(NOLOCK)
+		ON mdf.sis_id = vmm.sis_id
+		AND mdf.mod_id = vmm.mod_id
+		AND vis_id = @visaoUnidade
+	WHERE mdl.sis_id = @sis_id AND mdl.mod_nome = 'Administração' AND mdl.mod_situacao <> 3
+	
+	UPDATE SYS_VisaoModuloMenu
+	SET vmm_ordem = ISNULL(@vmm_ordem + 1, 1)
+	WHERE sis_id = @sis_id
+	AND vis_id = @visaoUnidade
+	AND mod_id = @mod_idPai
+	
+-- Fechar transação     
+SET XACT_ABORT OFF 
+COMMIT TRANSACTION
