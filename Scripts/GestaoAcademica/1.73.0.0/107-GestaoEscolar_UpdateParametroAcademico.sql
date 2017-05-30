1d80258a09cd4063e8de100b1cca4010eddd6453
Ana Cecilia Nascimento <ana.nascimento@mstech.com.br>
2017-05-30 11:41:46 -0300

    20000006-1256 [CIEJA] Permitir aparecer nos combos o CIEJA - Script de alteração do parâmetro

---
+++ Scripts/GestaoAcademica/1.73.0.0/107-GestaoEscolar_UpdateParametroAcademico.sql
@@ -0,0 +1,9 @@
+USE [GestaoPedagogica]
+GO
+
+UPDATE ACA_ParametroAcademico
+   SET pac_valor = ''
+	, pac_dataAlteracao = GETDATE()
+ WHERE pac_chave = 'TIPO_MODALIDADES_EJA_REMOVER_RELATORIO'
+
+GO
