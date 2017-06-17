638eb828cadd4da029b9395f9484ebdb25fe03eb
Daniel Jun Suguimoto <daniel.suguimoto@mstech.com.br>
2017-06-17 11:55:43 -0300

    20000006-1452 Adição de novas telas de lançamento

---
+++ Src/GestaoEscolar/Classe/RelatorioAtendimento/Busca.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeBehind="Busca.aspx.cs" Inherits="GestaoEscolar.Classe.RelatorioAtendimento.Busca" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ Src/GestaoEscolar/Classe/RelatorioAtendimento/Busca.aspx.cs
@@ -0,0 +1,18 @@
+﻿namespace GestaoEscolar.Classe.RelatorioAtendimento
+{
+    using MSTech.GestaoEscolar.Web.WebProject;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+
+    public partial class Busca : MotherPageLogado
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
