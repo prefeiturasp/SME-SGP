7a073e6a7dc4997da85d920ae37bfafc452d2433
marcia.haga <marcia.haga@MS-D2017>
2017-06-19 10:25:28 -0300

    20000006-1464 #320 - Adicionado tipo do tipo de disciplina diferenciado para AEE; implementado salvar dos tipos de disciplinas relacionados com a recuperação paralela.

--- Src/MSTech.GestaoEscolar.DAL/ACA_TipoDisciplinaRelacionadaDAO.cs
+++ Src/MSTech.GestaoEscolar.DAL/ACA_TipoDisciplinaRelacionadaDAO.cs
@@ -4,13 +4,43 @@
 
 namespace MSTech.GestaoEscolar.DAL
 {
-	using MSTech.GestaoEscolar.DAL.Abstracts;
-	
-	/// <summary>
-	/// Description: .
-	/// </summary>
-	public class ACA_TipoDisciplinaRelacionadaDAO : Abstract_ACA_TipoDisciplinaRelacionadaDAO
+    using Data.Common;
+    using MSTech.GestaoEscolar.DAL.Abstracts;
+    using System.Data;
+    using System.Data.SqlClient;
+
+    /// <summary>
+    /// Description: .
+    /// </summary>
+    public class ACA_TipoDisciplinaRelacionadaDAO : Abstract_ACA_TipoDisciplinaRelacionadaDAO
 	{
-		
-	}
+        /// <summary>
+        /// Remove os tipos de disciplina relacionados com um tipo de disciplina.
+        /// </summary>
+        /// <param name="tds_id"></param>
+		public void DeleteByTdsId(int tds_id)
+        {
+            QueryStoredProcedure qs = new QueryStoredProcedure("NEW_ACA_TipoDisciplinaRelacionada_DeleteByTdsId", _Banco);
+
+            try
+            {
+                #region Parâmetro
+
+                SqlParameter sqlParam = new SqlParameter();
+                sqlParam.DbType = DbType.Int32;
+                sqlParam.ParameterName = "@tds_id";
+                sqlParam.Size = 8;
+                sqlParam.Value = tds_id;
+                qs.Parameters.Add(sqlParam);
+
+                #endregion
+
+                qs.Execute();
+            }
+            finally
+            {
+                qs.Parameters.Clear();
+            }
+        }
+    }
 }
