ba7f7f97327f37b9ea9e9d5e68c26977cd0a3134
Daniel Jun Suguimoto <daniel.suguimoto@mstech.com.br>
2017-06-21 13:02:51 -0300

    20000006-1452 #311 Desenvolvimento de user control para lancçamento de relatórios

--- Src/GestaoEscolar/Includes/jsUCLancamentoRelatorioAtendimento.js
+++ Src/GestaoEscolar/Includes/jsUCLancamentoRelatorioAtendimento.js
@@ -1,7 +1,15 @@
 ﻿function jsUCLancamentoRelatorioAtendimento() {
-    createTabs("#divTabsRelatorio", "input[id$='txtSelectedTab']");
+    $(document).ready(function () {
+        createTabs("#divTabsRelatorio", "input[id$='txtSelectedTab']");
+        $('#divTabsRelatorio').tabs({ selected: parseInt($("input[id$='txtSelectedTab']").val()) });
+    });
 
-    $('#divTabsRelatorio').tabs({ selected: parseInt($("input[id$='txtSelectedTab']").val()) });
+    $("[type=radio]").each(function (i) {
+        var name = $(this).attr("name");
+        var splitted = name.split("$");
+        $(this).attr("name", splitted[splitted.length - 1]);
+    });
+
 }
 
 arrFNC.push(jsUCLancamentoRelatorioAtendimento);
