ea91014c806e2c1f21a2e4740789a8ffe6d16a6d
Daniel Jun Suguimoto <daniel.suguimoto@mstech.com.br>
2017-06-21 16:57:59 -0300

    20000006-1452 #311 Desenvolvimento de relatório de atendimento

---
+++ Src/GestaoEscolar/WebControls/Combos/UCComboRelatorioAtendimento.ascx.cs
@@ -0,0 +1,283 @@
+﻿using MSTech.GestaoEscolar.BLL;
+using MSTech.GestaoEscolar.Web.WebProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GestaoEscolar.WebControls.Combos
+{
+    public partial class UCComboRelatorioAtendimento : MotherUserControl
+    {
+        #region Delegates
+
+        public delegate void SelectedIndexChanged();
+
+        public event SelectedIndexChanged IndexChanged;
+
+        #endregion
+
+        #region Constantes
+
+        private const string valorSelecione = "-1";
+
+        #endregion
+
+        #region Proriedades
+
+        /// <summary>
+        /// ClientID do combo
+        /// </summary>
+        public string ClientID_Combo
+        {
+            get
+            {
+                return ddlCombo.ClientID;
+            }
+        }
+
+        /// <summary>
+        /// ClientID do validator
+        /// </summary>
+        public string ClientID_Validator
+        {
+            get
+            {
+                return cpvCombo.ClientID;
+            }
+        }
+
+        /// <summary>
+        /// ClientID do label
+        /// </summary>
+        public string ClientID_Label
+        {
+            get
+            {
+                return lblTitulo.ClientID;
+            }
+        }
+
+        /// <summary>
+        /// Coloca na primeira linha a mensagem de selecione um item.
+        /// </summary>
+        public bool MostrarMensagemSelecione
+        {
+            get
+            {
+                if (ViewState["MostrarMensagemSelecione"] != null)
+                    return Convert.ToBoolean(ViewState["MostrarMensagemSelecione"]);
+                return true;
+            }
+            set
+            {
+                ViewState["MostrarMensagemSelecione"] = value;
+            }
+        }
+
+        /// <summary>
+        /// Propriedade que seta a label e a validação do combo
+        /// </summary>
+        public bool Obrigatorio
+        {
+            set
+            {
+                if (value)
+                    AdicionaAsteriscoObrigatorio(lblTitulo);
+                else
+                    RemoveAsteriscoObrigatorio(lblTitulo);
+
+                cpvCombo.Visible = value;
+            }
+        }
+
+        /// <summary>
+        /// Deixa o combo habilitado de acordo com o valor passado
+        /// </summary>
+        public bool PermiteEditar
+        {
+            get
+            {
+                return ddlCombo.Enabled;
+            }
+            set
+            {
+                ddlCombo.Enabled = value;
+            }
+        }
+
+        /// <summary>
+        /// Propriedade que verifica quantos items existem no combo
+        /// </summary>
+        public int QuantidadeItensCombo
+        {
+            get
+            {
+                return ddlCombo.Items.Count;
+            }
+        }
+
+        /// <summary>
+        /// Propriedade que seta o SelectedIndex do Combo.       
+        /// </summary>
+        public int SelectedIndex
+        {
+            set
+            {
+                ddlCombo.SelectedValue = ddlCombo.Items[value].Value;
+            }
+        }
+
+        /// <summary>
+        /// Retorna o texto selecionado no combo
+        /// </summary>
+        public string Texto
+        {
+            get
+            {
+                return ddlCombo.SelectedItem.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Seta um titulo diferente do padrão para o combo
+        /// </summary>
+        public string Titulo
+        {
+            set
+            {
+                lblTitulo.Text = value;
+                cpvCombo.ErrorMessage = value.Replace('*', ' ') + " é obrigatório.";
+            }
+            get
+            {
+                return lblTitulo.Text;
+            }
+        }
+
+        /// <summary>
+        /// Seta o validationGroup do combo.
+        /// </summary>
+        public string ValidationGroup
+        {
+            set
+            {
+                cpvCombo.ValidationGroup = value;
+            }
+        }
+
+        /// <summary>
+        /// Retorna e seta o valor selecionado no combo
+        /// </summary>
+        public byte Valor
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(ddlCombo.SelectedValue))
+                    return 0;
+
+                return Convert.ToByte(ddlCombo.SelectedValue);
+            }
+            set
+            {
+                ddlCombo.SelectedValue = value.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Propriedade visible da label do nome do combo
+        /// </summary>
+        public bool Visible_Label
+        {
+            set
+            {
+                lblTitulo.Visible = value;
+            }
+        }
+
+        /// <summary>
+        /// Propriedade que seta o Width do combo.   
+        /// </summary>
+        public Int32 Width_Combo
+        {
+            set
+            {
+                ddlCombo.Width = value;
+            }
+        }
+
+        #endregion
+
+        #region Métodos
+
+        /// <summary>
+        /// Carrega a mensagem de selecione de acordo com o parâmetro
+        /// </summary>
+        private void CarregarMensagemSelecione()
+        {
+            if (MostrarMensagemSelecione && (ddlCombo.Items.FindByValue(valorSelecione) == null))
+                ddlCombo.Items.Insert(0, new ListItem("-- Selecione um tipo de relatório --", valorSelecione, true));
+
+            ddlCombo.AppendDataBoundItems = MostrarMensagemSelecione;
+        }
+
+        /// <summary>
+        /// Seta o foco no combo    
+        /// </summary>
+        public void SetarFoco()
+        {
+            ddlCombo.Focus();
+        }
+
+        public void CarregarPorPermissaoUuarioTipo(CLS_RelatorioAtendimentoTipo rea_tipo)
+        {
+            try
+            {
+                ddlCombo.Items.Clear();
+                ddlCombo.DataSource = CLS_RelatorioAtendimentoBO.SelecionaPorPermissaoUsuarioTipo(__SessionWEB.__UsuarioWEB.Usuario.usu_id, rea_tipo);
+                CarregarMensagemSelecione();
+
+                ddlCombo.DataBind();
+            }
+            catch (Exception)
+            {
+                lblMessage.Text = "Erro ao tentar carregar " + lblTitulo.Text.Replace('*', ' ').ToLower() + ".";
+                lblMessage.Visible = true;
+            }
+        }
+
+        #endregion
+
+        #region Eventos
+
+        #region Page Life Cycle
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            bool obrigatorio = lblTitulo.Text.EndsWith(ApplicationWEB.TextoAsteriscoObrigatorio) ||
+                               lblTitulo.Text.EndsWith(" *");
+
+            cpvCombo.ValueToCompare = valorSelecione;
+
+            Obrigatorio = obrigatorio;
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            ddlCombo.AutoPostBack = (IndexChanged != null);
+            CarregarMensagemSelecione();
+        }
+
+        #endregion
+
+        protected void ddlCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (IndexChanged != null)
+                IndexChanged();
+        }
+
+        #endregion
+    }
+}
