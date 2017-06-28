2ddc300c596850418a9b984245c0bc02ae6116a1
marcia.haga <marcia.haga@MS-D2017>
2017-06-28 09:28:18 -0300

    20000006-1656 #331 - Ajuste na chave primária da tabela CLS_RelatorioPreenchimentoAcoesRealizadas.

--- Src/MSTech.GestaoEscolar.DAL/Abstracts/Abstract_CLS_RelatorioPreenchimentoAcoesRealizadasDAO.cs
+++ Src/MSTech.GestaoEscolar.DAL/Abstracts/Abstract_CLS_RelatorioPreenchimentoAcoesRealizadasDAO.cs
@@ -4,17 +4,17 @@
 
 namespace MSTech.GestaoEscolar.DAL.Abstracts
 {
-	using System;
-	using System.Data;
-	using MSTech.Data.Common;
-	using MSTech.Data.Common.Abstracts;
-	using MSTech.GestaoEscolar.Entities;
-	
-	/// <summary>
-	/// Classe abstrata de CLS_RelatorioPreenchimentoAcoesRealizadas.
-	/// </summary>
-	public abstract class Abstract_CLS_RelatorioPreenchimentoAcoesRealizadasDAO : Abstract_DAL<CLS_RelatorioPreenchimentoAcoesRealizadas>
-	{
+    using System;
+    using System.Data;
+    using MSTech.Data.Common;
+    using MSTech.Data.Common.Abstracts;
+    using MSTech.GestaoEscolar.Entities;
+
+    /// <summary>
+    /// Classe abstrata de CLS_RelatorioPreenchimentoAcoesRealizadas.
+    /// </summary>
+    public abstract class Abstract_CLS_RelatorioPreenchimentoAcoesRealizadasDAO : Abstract_DAL<CLS_RelatorioPreenchimentoAcoesRealizadas>
+    {
         /// <summary>
 		/// ConnectionString.
 		/// </summary>
@@ -25,199 +25,193 @@ namespace MSTech.GestaoEscolar.DAL.Abstracts
                 return "GestaoEscolar";
             }
         }
-        	
-		/// <summary>
-		/// Configura os parametros do metodo de carregar.
-		/// </summary>
-		/// <param name="qs">Objeto da Store Procedure.</param>
+
+        /// <summary>
+        /// Configura os parametros do metodo de carregar.
+        /// </summary>
+        /// <param name="qs">Objeto da Store Procedure.</param>
         /// <param name="entity">Entidade com os dados para preenchimento dos parametros.</param>
-		protected override void ParamCarregar(QuerySelectStoredProcedure qs, CLS_RelatorioPreenchimentoAcoesRealizadas entity)
-		{
-			if (entity != null & qs != null)
+        protected override void ParamCarregar(QuerySelectStoredProcedure qs, CLS_RelatorioPreenchimentoAcoesRealizadas entity)
+        {
+            if (entity != null & qs != null)
             {
-			Param = qs.NewParameter();
-			Param.DbType = DbType.Int64;
-			Param.ParameterName = "@rpa_id";
-			Param.Size = 8;
-			Param.Value = entity.rpa_id;
-			qs.Parameters.Add(Param);
+                Param = qs.NewParameter();
+                Param.DbType = DbType.Int64;
+                Param.ParameterName = "@rpa_id";
+                Param.Size = 8;
+                Param.Value = entity.rpa_id;
+                qs.Parameters.Add(Param);
 
 
-			}
-		}
-		
-		/// <summary>
-		/// Configura os parametros do metodo de Inserir.
-		/// </summary>
-		/// <param name="qs">Objeto da Store Procedure.</param>
+            }
+        }
+
+        /// <summary>
+        /// Configura os parametros do metodo de Inserir.
+        /// </summary>
+        /// <param name="qs">Objeto da Store Procedure.</param>
         /// <param name="entity">Entidade com os dados para preenchimento dos parametros.</param>
-		protected override void ParamInserir(QuerySelectStoredProcedure qs, CLS_RelatorioPreenchimentoAcoesRealizadas entity)
-		{
-			if (entity != null & qs != null)
+        protected override void ParamInserir(QuerySelectStoredProcedure qs, CLS_RelatorioPreenchimentoAcoesRealizadas entity)
+        {
+            if (entity != null & qs != null)
             {
-							Param = qs.NewParameter();
-			Param.DbType = DbType.Int64;
-			Param.ParameterName = "@rpa_id";
-			Param.Size = 8;
-			Param.Value = entity.rpa_id;
-			qs.Parameters.Add(Param);
+                Param = qs.NewParameter();
+                Param.DbType = DbType.Int64;
+                Param.ParameterName = "@reap_id";
+                Param.Size = 8;
+                Param.Value = entity.reap_id;
+                qs.Parameters.Add(Param);
 
-			Param = qs.NewParameter();
-			Param.DbType = DbType.Int64;
-			Param.ParameterName = "@reap_id";
-			Param.Size = 8;
-			Param.Value = entity.reap_id;
-			qs.Parameters.Add(Param);
+                Param = qs.NewParameter();
+                Param.DbType = DbType.Date;
+                Param.ParameterName = "@rpa_data";
 
-			Param = qs.NewParameter();
-			Param.DbType = DbType.Date;
-			Param.ParameterName = "@rpa_data";
+                Param.Value = entity.rpa_data;
+                qs.Parameters.Add(Param);
 
-			Param.Value = entity.rpa_data;
-			qs.Parameters.Add(Param);
+                Param = qs.NewParameter();
+                Param.DbType = DbType.Boolean;
+                Param.ParameterName = "@rpa_impressao";
+                Param.Size = 1;
+                Param.Value = entity.rpa_impressao;
+                qs.Parameters.Add(Param);
 
-			Param = qs.NewParameter();
-			Param.DbType = DbType.Boolean;
-			Param.ParameterName = "@rpa_impressao";
-			Param.Size = 1;
-			Param.Value = entity.rpa_impressao;
-			qs.Parameters.Add(Param);
+                Param = qs.NewParameter();
+                Param.DbType = DbType.String;
+                Param.ParameterName = "@rpa_acao";
 
-			Param = qs.NewParameter();
-			Param.DbType = DbType.String;
-			Param.ParameterName = "@rpa_acao";
+                Param.Value = entity.rpa_acao;
+                qs.Parameters.Add(Param);
 
-			Param.Value = entity.rpa_acao;
-			qs.Parameters.Add(Param);
+                Param = qs.NewParameter();
+                Param.DbType = DbType.Byte;
+                Param.ParameterName = "@rpa_situacao";
+                Param.Size = 1;
+                Param.Value = entity.rpa_situacao;
+                qs.Parameters.Add(Param);
 
-			Param = qs.NewParameter();
-			Param.DbType = DbType.Byte;
-			Param.ParameterName = "@rpa_situacao";
-			Param.Size = 1;
-			Param.Value = entity.rpa_situacao;
-			qs.Parameters.Add(Param);
+                Param = qs.NewParameter();
+                Param.DbType = DbType.DateTime;
+                Param.ParameterName = "@rpa_dataCriacao";
 
-			Param = qs.NewParameter();
-			Param.DbType = DbType.DateTime;
-			Param.ParameterName = "@rpa_dataCriacao";
+                Param.Value = entity.rpa_dataCriacao;
+                qs.Parameters.Add(Param);
 
-			Param.Value = entity.rpa_dataCriacao;
-			qs.Parameters.Add(Param);
+                Param = qs.NewParameter();
+                Param.DbType = DbType.DateTime;
+                Param.ParameterName = "@rpa_dataAlteracao";
 
-			Param = qs.NewParameter();
-			Param.DbType = DbType.DateTime;
-			Param.ParameterName = "@rpa_dataAlteracao";
-
-			Param.Value = entity.rpa_dataAlteracao;
-			qs.Parameters.Add(Param);
+                Param.Value = entity.rpa_dataAlteracao;
+                qs.Parameters.Add(Param);
 
 
-			}
-		}
-		
-		/// <summary>
-		/// Configura os parametros do metodo de Alterar.
-		/// </summary>
-		/// <param name="qs">Objeto da Store Procedure.</param>
+            }
+        }
+
+        /// <summary>
+        /// Configura os parametros do metodo de Alterar.
+        /// </summary>
+        /// <param name="qs">Objeto da Store Procedure.</param>
         /// <param name="entity">Entidade com os dados para preenchimento dos parametros.</param>
-		protected override void ParamAlterar(QueryStoredProcedure qs, CLS_RelatorioPreenchimentoAcoesRealizadas entity)
-		{
-			if (entity != null & qs != null)
+        protected override void ParamAlterar(QueryStoredProcedure qs, CLS_RelatorioPreenchimentoAcoesRealizadas entity)
+        {
+            if (entity != null & qs != null)
             {
-			Param = qs.NewParameter();
-			Param.DbType = DbType.Int64;
-			Param.ParameterName = "@rpa_id";
-			Param.Size = 8;
-			Param.Value = entity.rpa_id;
-			qs.Parameters.Add(Param);
+                Param = qs.NewParameter();
+                Param.DbType = DbType.Int64;
+                Param.ParameterName = "@rpa_id";
+                Param.Size = 8;
+                Param.Value = entity.rpa_id;
+                qs.Parameters.Add(Param);
 
-			Param = qs.NewParameter();
-			Param.DbType = DbType.Int64;
-			Param.ParameterName = "@reap_id";
-			Param.Size = 8;
-			Param.Value = entity.reap_id;
-			qs.Parameters.Add(Param);
+                Param = qs.NewParameter();
+                Param.DbType = DbType.Int64;
+                Param.ParameterName = "@reap_id";
+                Param.Size = 8;
+                Param.Value = entity.reap_id;
+                qs.Parameters.Add(Param);
 
-			Param = qs.NewParameter();
-			Param.DbType = DbType.Date;
-			Param.ParameterName = "@rpa_data";
+                Param = qs.NewParameter();
+                Param.DbType = DbType.Date;
+                Param.ParameterName = "@rpa_data";
 
-			Param.Value = entity.rpa_data;
-			qs.Parameters.Add(Param);
+                Param.Value = entity.rpa_data;
+                qs.Parameters.Add(Param);
 
-			Param = qs.NewParameter();
-			Param.DbType = DbType.Boolean;
-			Param.ParameterName = "@rpa_impressao";
-			Param.Size = 1;
-			Param.Value = entity.rpa_impressao;
-			qs.Parameters.Add(Param);
+                Param = qs.NewParameter();
+                Param.DbType = DbType.Boolean;
+                Param.ParameterName = "@rpa_impressao";
+                Param.Size = 1;
+                Param.Value = entity.rpa_impressao;
+                qs.Parameters.Add(Param);
 
-			Param = qs.NewParameter();
-			Param.DbType = DbType.String;
-			Param.ParameterName = "@rpa_acao";
+                Param = qs.NewParameter();
+                Param.DbType = DbType.String;
+                Param.ParameterName = "@rpa_acao";
 
-			Param.Value = entity.rpa_acao;
-			qs.Parameters.Add(Param);
+                Param.Value = entity.rpa_acao;
+                qs.Parameters.Add(Param);
 
-			Param = qs.NewParameter();
-			Param.DbType = DbType.Byte;
-			Param.ParameterName = "@rpa_situacao";
-			Param.Size = 1;
-			Param.Value = entity.rpa_situacao;
-			qs.Parameters.Add(Param);
+                Param = qs.NewParameter();
+                Param.DbType = DbType.Byte;
+                Param.ParameterName = "@rpa_situacao";
+                Param.Size = 1;
+                Param.Value = entity.rpa_situacao;
+                qs.Parameters.Add(Param);
 
-			Param = qs.NewParameter();
-			Param.DbType = DbType.DateTime;
-			Param.ParameterName = "@rpa_dataCriacao";
+                Param = qs.NewParameter();
+                Param.DbType = DbType.DateTime;
+                Param.ParameterName = "@rpa_dataCriacao";
 
-			Param.Value = entity.rpa_dataCriacao;
-			qs.Parameters.Add(Param);
+                Param.Value = entity.rpa_dataCriacao;
+                qs.Parameters.Add(Param);
 
-			Param = qs.NewParameter();
-			Param.DbType = DbType.DateTime;
-			Param.ParameterName = "@rpa_dataAlteracao";
+                Param = qs.NewParameter();
+                Param.DbType = DbType.DateTime;
+                Param.ParameterName = "@rpa_dataAlteracao";
 
-			Param.Value = entity.rpa_dataAlteracao;
-			qs.Parameters.Add(Param);
+                Param.Value = entity.rpa_dataAlteracao;
+                qs.Parameters.Add(Param);
 
 
-			}
-		}
+            }
+        }
 
-		/// <summary>
-		/// Configura os parametros do metodo de Deletar.
-		/// </summary>
-		/// <param name="qs">Objeto da Store Procedure.</param>
+        /// <summary>
+        /// Configura os parametros do metodo de Deletar.
+        /// </summary>
+        /// <param name="qs">Objeto da Store Procedure.</param>
         /// <param name="entity">Entidade com os dados para preenchimento dos parametros.</param>
-		protected override void ParamDeletar(QueryStoredProcedure qs, CLS_RelatorioPreenchimentoAcoesRealizadas entity)
-		{
-			if (entity != null & qs != null)
+        protected override void ParamDeletar(QueryStoredProcedure qs, CLS_RelatorioPreenchimentoAcoesRealizadas entity)
+        {
+            if (entity != null & qs != null)
             {
-			Param = qs.NewParameter();
-			Param.DbType = DbType.Int64;
-			Param.ParameterName = "@rpa_id";
-			Param.Size = 8;
-			Param.Value = entity.rpa_id;
-			qs.Parameters.Add(Param);
+                Param = qs.NewParameter();
+                Param.DbType = DbType.Int64;
+                Param.ParameterName = "@rpa_id";
+                Param.Size = 8;
+                Param.Value = entity.rpa_id;
+                qs.Parameters.Add(Param);
 
 
-			}
-		}
-		
-		/// <summary>
-		/// Recebe o valor do auto incremento e coloca na propriedade.
-		/// </summary>
-		/// <param name="qs">Objeto da Store Procedure.</param>
-		/// <param name="entity">Entidade com os dados para preenchimento dos parametros.</param>
-		/// <returns>TRUE - Se entity.ParametroId > 0</returns>
-		protected override bool ReceberAutoIncremento(QuerySelectStoredProcedure qs, CLS_RelatorioPreenchimentoAcoesRealizadas entity)
-		{
-			if (entity != null & qs != null)
+            }
+        }
+
+        /// <summary>
+        /// Recebe o valor do auto incremento e coloca na propriedade.
+        /// </summary>
+        /// <param name="qs">Objeto da Store Procedure.</param>
+        /// <param name="entity">Entidade com os dados para preenchimento dos parametros.</param>
+        /// <returns>TRUE - Se entity.ParametroId > 0</returns>
+        protected override bool ReceberAutoIncremento(QuerySelectStoredProcedure qs, CLS_RelatorioPreenchimentoAcoesRealizadas entity)
+        {
+            if (entity != null & qs != null)
             {
-				throw new NotImplementedException("Método 'ReceberAutoIncremento' não implementado.");
-			}
+                entity.rpa_id = Convert.ToInt32(qs.Return.Rows[0][0]);
+                return (entity.rpa_id > 0);
+            }
 
-			return false;
-		}		
-	}
+            return false;
+        }
+    }
 }
