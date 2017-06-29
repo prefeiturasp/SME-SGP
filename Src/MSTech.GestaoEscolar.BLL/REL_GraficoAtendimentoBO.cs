9b0db3efb79e648489f20a5a41214c8f7bd07638
debora.santos <debora.santos@mstech.com.br>
2017-06-29 17:28:42 -0300

    20000006-1650 #334 - (filha #283) NAAPA/AEE - Cadastro de gráficos de atendimento - Desenvolvimento

--- Src/MSTech.GestaoEscolar.BLL/REL_GraficoAtendimentoBO.cs
+++ Src/MSTech.GestaoEscolar.BLL/REL_GraficoAtendimentoBO.cs
@@ -9,6 +9,46 @@ namespace MSTech.GestaoEscolar.BLL
     using MSTech.GestaoEscolar.DAL;
     using System.Collections.Generic;
     using System.Data;
+    using System.ComponentModel;
+
+    public enum REL_GraficoAtendimentoTipo : byte
+    {
+        [Description("REL_GraficoAtendimentoBO.REL_GraficoAtendimentoTipo.Barra")]
+        Barra = 1
+        ,
+        [Description("REL_GraficoAtendimentoBO.REL_GraficoAtendimentoTipo.Pizza")]
+        Pizza = 2
+    }
+
+    public enum REL_GraficoAtendimentoEixoAgrupamento : byte
+    {
+        [Description("REL_GraficoAtendimentoBO.REL_GraficoAtendimentoEixoAgrupamento.Curso")]
+        Curso = 1
+        ,
+        [Description("REL_GraficoAtendimentoBO.REL_GraficoAtendimentoEixoAgrupamento.Ciclo")]
+        Ciclo = 2
+        ,
+        [Description("REL_GraficoAtendimentoBO.REL_GraficoAtendimentoEixoAgrupamento.PeridoCurso")]
+        PeridoCurso = 3
+    }
+    
+    public enum REL_GraficoAtendimentoFiltrosFixos : byte
+    {
+        [Description("REL_GraficoAtendimentoBO.REL_GraficoAtendimentoFiltrosFixos.PeriodoPreenchimento")]
+        PeriodoPreenchimento = 1
+        ,
+        [Description("REL_GraficoAtendimentoBO.REL_GraficoAtendimentoFiltrosFixos.RacaCor")]
+        RacaCor = 2
+        ,
+        [Description("REL_GraficoAtendimentoBO.REL_GraficoAtendimentoFiltrosFixos.FaixaIdade")]
+        FaixaIdade = 3
+        ,
+        [Description("REL_GraficoAtendimentoBO.REL_GraficoAtendimentoFiltrosFixos.Sexo")]
+        Sexo = 4
+        ,
+        [Description("REL_GraficoAtendimentoBO.REL_GraficoAtendimentoFiltrosFixos.DetalheDeficiencia")]
+        DetalheDeficiencia = 5
+    }
 
     /// <summary>
     /// Description: REL_GraficoAtendimento Business Object. 
