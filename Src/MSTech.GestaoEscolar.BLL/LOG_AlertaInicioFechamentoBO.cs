feffd1ea16635d75c59be5e3a3b067b5c7a87530
marcia.haga <marcia.haga@MS-D2017>
2017-07-12 10:42:57 -0300

    20000006-1645 #65 - Adicionado log do envio do alerta de abertura do fechamento.

--- Src/MSTech.GestaoEscolar.BLL/LOG_AlertaInicioFechamentoBO.cs
+++ Src/MSTech.GestaoEscolar.BLL/LOG_AlertaInicioFechamentoBO.cs
@@ -8,6 +8,8 @@ namespace MSTech.GestaoEscolar.BLL
     using MSTech.GestaoEscolar.Entities;
     using MSTech.GestaoEscolar.DAL;
     using System;
+    using System.Collections.Generic;
+    using System.Data;
 
     #region Estruturas
 
@@ -29,7 +31,35 @@ namespace MSTech.GestaoEscolar.BLL
     /// Description: LOG_AlertaInicioFechamento Business Object. 
     /// </summary>
     public class LOG_AlertaInicioFechamentoBO : BusinessBase<LOG_AlertaInicioFechamentoDAO, LOG_AlertaInicioFechamento>
-	{
-				
-	}
+    {
+        /// <summary>
+        /// Salva o log de envio da notificação em lote.
+        /// </summary>
+        /// <param name="lstLog"></param>
+        /// <returns></returns>
+        public static bool SalvarEmLote(List<LOG_AlertaInicioFechamento> lstLog)
+        {
+            DataTable dtLog = LOG_AlertaInicioFechamento.TipoTabela_LOG_AlertaInicioFechamento();
+            lstLog.ForEach(p =>
+            {
+                dtLog.Rows.Add(LogToDataRow(p, dtLog.NewRow()));
+            });
+            new LOG_AlertaInicioFechamentoDAO().SalvarEmLote(dtLog);
+            return true;
+        }
+
+        /// <summary>
+        /// Retorna um datarow com dados da entidade.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="dr"></param>
+        /// <returns></returns>
+        private static DataRow LogToDataRow(LOG_AlertaInicioFechamento entity, DataRow dr)
+        {
+            dr["usu_id"] = entity.usu_id;
+            dr["evt_id"] = entity.evt_id;
+            dr["lif_dataEnvio"] = entity.lif_dataEnvio;
+            return dr;
+        }
+    }
 }
