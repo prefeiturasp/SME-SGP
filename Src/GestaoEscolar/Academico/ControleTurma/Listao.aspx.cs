d392d8350e5fd415c5ddfacb1b15f71512362b71
Leonardo Andrade Brito <leonardo.brito@mstech.com.br>
2017-07-18 09:20:16 -0300

    Ajuste na exibição de pendência de plano de aula. #20000006-1914 #271

--- Src/GestaoEscolar/Academico/ControleTurma/Listao.aspx.cs
+++ Src/GestaoEscolar/Academico/ControleTurma/Listao.aspx.cs
@@ -4191,7 +4191,7 @@ namespace GestaoEscolar.Academico.ControleTurma
                 if (imgSemPlanoAula != null && hdfSemPlanoAula != null &&
                     Convert.ToDateTime(lblData.Text).Date < DateTime.Now.Date)
                 {
-                    imgSemPlanoAula.Visible = Convert.ToBoolean(hdfSemPlanoAula.Value)
+                    imgSemPlanoAula.Visible = Convert.ToBoolean(hdfSemPlanoAula.Value) && ACA_ParametroAcademicoBO.ParametroValorBooleanoPorEntidade(eChaveAcademico.EXIBIR_ALERTA_AULA_SEM_PLANO, __SessionWEB.__UsuarioWEB.Usuario.ent_id)
                                                 && (__SessionWEB.__UsuarioWEB.Grupo.vis_id == SysVisaoID.Individual
                                                     || VS_EntitiesControleTurma.curso.tne_id != ACA_ParametroAcademicoBO.ParametroValorInt32PorEntidade(eChaveAcademico.TIPO_NIVEL_ENSINO_EDUCACAO_INFANTIL, __SessionWEB.__UsuarioWEB.Usuario.ent_id)
                                                     || ACA_ParametroAcademicoBO.ParametroValorBooleanoPorEntidade(eChaveAcademico.EXIBIR_ALERTA_AULA_SEM_PLANO_ENSINO_INFANTIL, __SessionWEB.__UsuarioWEB.Usuario.ent_id));
