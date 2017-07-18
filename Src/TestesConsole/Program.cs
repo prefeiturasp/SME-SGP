52edef8051edd90da59cbfdd9bbe5728285696ce
Carla Frascareli <carla.frascareli@mstech.com.br>
2017-07-18 09:43:34 -0300

    Ajuste serviços

--- Src/TestesConsole/Program.cs
+++ Src/TestesConsole/Program.cs
@@ -13,24 +13,43 @@ namespace TestesConsole
         {
             try
             {
-                while (Console.ReadLine().ToLower() == "e")
+                string letra = Console.ReadLine().ToLower();
+                while (letra.In("e", "w", "p"))
                 {
-                    Console.WriteLine("Processando fechamento...");
-                    GestaoEscolarServicosBO.ExecJOB_ProcessamentoNotaFrequenciaFechamentoAsync(limpacache: false);
-                    Console.WriteLine("Processando pendências...");
-                    GestaoEscolarServicosBO.ExecJOB_ProcessamentoRelatorioDisciplinasAlunosPendenciasAsync(limpacache: false);
-                    Console.WriteLine("Processando preenchimento de frequência...");
-                    GestaoEscolarServicosBO.ExecJOB_ProcessamentoPreenchimentoFrequencia();
-                    Console.WriteLine("Processando alunos com baixa frequência e com faltas consecutivas...");
-                    GestaoEscolarServicosBO.ExecJOB_ProcessamentoAlunosFrequencia();
-                    Console.WriteLine("Finalizado.");
+                    if (letra == "e")
+                    {
+                        Console.WriteLine("Processando fechamento...");
+                        GestaoEscolarServicosBO.ExecJOB_ProcessamentoNotaFrequenciaFechamentoAsync(limpacache: false);
+                        Console.WriteLine("Processando pendências...");
+                        GestaoEscolarServicosBO.ExecJOB_ProcessamentoRelatorioDisciplinasAlunosPendenciasAsync(limpacache: false);
+                        Console.WriteLine("Processando preenchimento de frequência...");
+                        GestaoEscolarServicosBO.ExecJOB_ProcessamentoPreenchimentoFrequencia();
+                        Console.WriteLine("Processando alunos com baixa frequência e com faltas consecutivas...");
+                        GestaoEscolarServicosBO.ExecJOB_ProcessamentoAlunosFrequencia();
+                        Console.WriteLine("Finalizado.");
+                    }
+                    else if (letra == "w")
+                    {
+                        Console.WriteLine("Processando alerta de início de fechamento...");
+                        GestaoEscolarServicosBO.ExecJOB_AlertaInicioFechamento();
+                        Console.WriteLine("Processando alerta de fim de fechamento...");
+                        GestaoEscolarServicosBO.ExecJOB_AlertaFimFechamento();
+                        Console.WriteLine("Finalizado.");
+                    }
+                    else if (letra == "p")
+                    {
+                        Console.WriteLine("Processando pendência de aula...");
+                        GestaoEscolarServicosBO.ExecJOB_ProcessamentoPendenciaAulas();
+                        Console.WriteLine("Finalizado.");
+                    }
+
+                    letra = Console.ReadLine().ToLower();
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Erro: " + ex.Message);
                 Console.ReadLine();
-                //Util.GravarErro(ex, context.Scheduler.Context);
             }
         }
     }
