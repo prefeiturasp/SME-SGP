740085dbeb449909e5c4c881bda2eff53bb3768c
Bruno Valle <bwvalle@yahoo.com.br>
2017-08-29 16:56:17 -0300

    Atualizando as configuraÃ§Ãµes do build

--- build/upload-nexus-raw.ps1
+++ build/upload-nexus-raw.ps1
@@ -17,5 +17,7 @@ $Headers = @{
 $fileToUpload = (ls .\output\*zip)
 $uploadUrl="$nexusUrl/repository/$projectRepository/$($fileToUpload.Name)"
 
+Write-Host "Nexus: $uploadUrl"
+
 Invoke-RestMethod -Uri $uploadUrl -Headers $Headers -Method Put -InFile $fileToUpload.FullName
 
